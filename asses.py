e5f833dee7d96a2214bd42b3fddbdc346b4d9ad7
lynkas <cat@catting.net>
2018-09-16 21:33:23 +0800

    remove test output

--- asses.py
+++ asses.py
@@ -27,7 +27,7 @@ class an_ass:
 
 class time:
     def __init__(self,t):
-        # print(t)
+        # #print(t)
         self.h = int(t.split(':')[0])
         self.m = int(t.split(':')[1])
         self.s = int(t.split(':')[2].split('.')[0])
@@ -61,24 +61,24 @@ class dia:
 def isRe(t1s,t1e,t2s,t2e):
     d=(min(t1e.rms,t2e.rms)-max(t1s.rms,t2s.rms))
     if d/(t1e.rms-t1s.rms)>=R and d/(t2e.rms-t2s.rms)>=R:
-        print(3)
+        #print(3)
         return 3
     elif d/(t1e.rms-t1s.rms)>=R and d/(t2e.rms-t2s.rms)<R:
-        print(1)
+        #print(1)
         return 1
     elif d/(t1e.rms-t1s.rms)<R and d/(t2e.rms-t2s.rms)>=R:
-        print(2)
+        #print(2)
         return 2
     else:
-        print(0)
+        #print(0)
         return 0
 
 
 
-# u=sys.argv[1]
-# d=sys.argv[2]
-u='zh.ass'
-d='en.ass'
+u=sys.argv[1]
+d=sys.argv[2]
+# u='zh.ass'
+# d='en.ass'
 a = an_ass(u,'u')
 b = an_ass(d,'d')
 uplist=[]
@@ -105,29 +105,29 @@ def comb(lis1,lis2,res):
 
     if re==0:
         if (lis1[0].st.rms < lis2[0].st.rms):
-            print(lis1[0])
+            #print(lis1[0])
             res.append(lis1.pop(0))
         else:
-            print(lis2[0])
+            #print(lis2[0])
             if las.isLagerThan(lis2[0].st):
                 lis2[0].st=las
             las = lis2[0].et
-            print(str(las))
+            #print(str(las))
             res.append(lis2.pop(0))
 
     elif re==1:
         if len(lis1)!=1 and isRe(lis1[0].st, lis1[1].et, lis2[0].st, lis2[0].et)==3:
             lis2[0].st=lis1[0].st
             lis2[0].et=lis1[1].et
-            print(lis2[0])
+            #print(lis2[0])
             if las.isLagerThan(lis2[0].st):
                 lis2[0].st=las
             las=lis2[0].et
-            print(str(las))
+            #print(str(las))
             res.append(lis2.pop(0))
-            print(lis1[0])
+            #print(lis1[0])
             res.append(lis1.pop(0))
-            print(lis1[0])
+            #print(lis1[0])
             res.append(lis1.pop(0))
         else:
             lis2[0].st=lis1[0].st
@@ -137,15 +137,15 @@ def comb(lis1,lis2,res):
         if len(lis2)!=1 and isRe(lis1[0].st, lis1[0].et, lis2[0].st, lis2[1].et)==3:
             lis2[0].st=lis1[0].st
             lis2[1].et=lis1[0].et
-            print(lis1[0])
+            #print(lis1[0])
             res.append(lis1.pop(0))
-            print(lis2[0])
+            #print(lis2[0])
             res.append(lis2.pop(0))
             if las.isLagerThan(lis2[0].st):
                 lis2[0].st=las
-            print(lis2[0])
+            #print(lis2[0])
             las = lis2[0].et
-            print(str(las))
+            #print(str(las))
             res.append(lis2.pop(0))
         else:
             lis2[0].st=lis1[0].st
@@ -153,13 +153,13 @@ def comb(lis1,lis2,res):
     else:
         lis2[0].st=lis1[0].st
         lis2[0].et=lis1[0].et
-        print(lis1[0])
+        #print(lis1[0])
         res.append(lis1.pop(0))
-        print(lis2[0])
+        #print(lis2[0])
         if las.isLagerThan(lis2[0].st):
             lis2[0].st = las
         las = lis2[0].et
-        print(str(las))
+        #print(str(las))
         res.append(lis2.pop(0))
 
     comb(lis1,lis2,res)
